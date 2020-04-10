261d87df3660f164baf844f729215d18d455e060
Ehsanur Rahman Rhythm <40324252+errhythm@users.noreply.github.com>
2020-04-10 18:19:45 +0600

    Create A.java
    LightOJ - 1000

---
+++ A.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+  
+  public static void main(String[] args) {
+    
+    Scanner sc=new Scanner(System.in);
+    int x=sc.nextInt();
+    int count=1;
+    int sum=0;
+    
+    
+    while (count<=x) {
+      
+      
+      
+      int r =sc.nextInt();
+      int s =sc.nextInt();
+      sum=r+s;
+      System.out.println("Case "+count+": "+sum);
+      count++;
+      
+    }
+  }
+}
